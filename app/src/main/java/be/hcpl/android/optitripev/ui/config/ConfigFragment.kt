f89db9adcea7a553f785df2b619d3fed62b915fc
JD41256 <hans.cappelle@kbc.be>
2022-05-13 14:16:20 +0200

    display config (wip)

--- app/src/main/java/be/hcpl/android/optitripev/ui/config/ConfigFragment.kt
+++ app/src/main/java/be/hcpl/android/optitripev/ui/config/ConfigFragment.kt
@@ -4,35 +4,89 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import be.hcpl.android.optitripev.databinding.FragmentConfigBinding
+import be.hcpl.android.optitripev.ui.result.ResultViewModel
 
 class ConfigFragment : Fragment() {
 
     private var _binding: FragmentConfigBinding? = null
+    private lateinit var viewModel: ConfigViewModel
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycle.addObserver(viewModel)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(ConfigViewModel::class.java)
+
+        viewModel = ViewModelProvider(this).get(ConfigViewModel::class.java)
 
         _binding = FragmentConfigBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //val textView: TextView = binding.textNotifications
-        //notificationsViewModel.text.observe(viewLifecycleOwner) {
-        //    textView.text = it
-        //}
+        val configContainerView = binding.configContainer
+
+        // TODO create a screen here with multiple input options + recover option + later on also profiles
+        configContainerView.addView(createRow("Speed","Wh/km", "kWh/km" ))
+        viewModel.speedValues.observe(viewLifecycleOwner){
+            it.forEachIndexed { index, key ->
+                val value = viewModel.consumptionValues.value?.get(index)
+                configContainerView.addView(createEditableRow(key.toString(), (value?.times(1000)).toString(), value.toString() ))
+            }
+        }
+
         return root
     }
+
+    private fun createEditableRow(label1: String, input1: String, label2: String) : ViewGroup {
+        val viewGroup = LinearLayout(context)
+        viewGroup.orientation = LinearLayout.HORIZONTAL
+        val tv1 = TextView(context)
+        tv1.text = label1
+        tv1.layoutParams = params
+        viewGroup.addView(tv1)
+        val edit2 = EditText(context)
+        edit2.setText(input1)
+        edit2.layoutParams = params
+        viewGroup.addView(edit2)
+        val tv3 = TextView(context)
+        tv3.text = label2
+        tv3.layoutParams = params
+        viewGroup.addView(tv3)
+        return viewGroup
+
+    }
+
+    private fun createRow(label1: String, label2: String, label3: String) : ViewGroup {
+        val viewGroup = LinearLayout(context)
+        viewGroup.orientation = LinearLayout.HORIZONTAL
+        val tv1 = TextView(context)
+        tv1.text = label1
+        tv1.layoutParams = params
+        viewGroup.addView(tv1)
+        val tv2 = TextView(context)
+        tv2.text = label2
+        tv2.layoutParams = params
+        viewGroup.addView(tv2)
+        val tv3 = TextView(context)
+        tv3.text = label3
+        tv3.layoutParams = params
+        viewGroup.addView(tv3)
+        return viewGroup
+    }
+
+    private val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f)
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/be/hcpl/android/optitripev/ui/config/ConfigViewModel.kt
+++ app/src/main/java/be/hcpl/android/optitripev/ui/config/ConfigViewModel.kt
@@ -1,13 +1,35 @@
 package be.hcpl.android.optitripev.ui.config
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import be.hcpl.android.optitripev.util.Constants
 
-class ConfigViewModel : ViewModel() {
+class ConfigViewModel(application: Application) : AndroidViewModel(application),
+    DefaultLifecycleObserver {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    private val context by lazy { getApplication<Application>().applicationContext }
+    private val prefs = context.getSharedPreferences(Constants.APP_PREFERENCES, Context.MODE_PRIVATE)
+
+    // TODO allow for endless entries? In case user wants to add different speeds?
+    val speedValues = MutableLiveData<List<Int>>()
+    val consumptionValues = MutableLiveData<List<Double>>()
+
+    override fun onPause(lifecycleOwner: LifecycleOwner){
+
     }
-    val text: LiveData<String> = _text
+
+    override fun onResume(lifecycleOwner: LifecycleOwner){
+        // TODO move this table to a configurable view so users can have their own
+        consumptionValues.value = Constants.defaultSpeedByConsumption.values.toList()
+        speedValues.value = Constants.defaultSpeedByConsumption.keys.toList()
+
+    }
+
+
 }
