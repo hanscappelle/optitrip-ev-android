25db8af84c627e18d8dacea8bfb58720d24738d0
Hans Cappelle <hans@roov.app>
2025-03-02 13:09:36 +0100

    scroll on result screen

--- app/src/main/java/be/hcpl/android/optitripev/ui/screen/ResultScreen.kt
+++ app/src/main/java/be/hcpl/android/optitripev/ui/screen/ResultScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +30,7 @@ fun ResultScreen(
         verticalArrangement = spacedBy(8.dp),
         modifier = Modifier
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
             .fillMaxSize(),
     ) {
         // info
